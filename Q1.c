8f2b044265ddbfe31ddaf013f5d8aefed216534f
boa3444 <newone1804@gmail.com>
2025-08-22 05:26:16 +0000

    DAY-1

---
+++ Q1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	int sum = 0;
+	int a,b;
+	printf("Type 1st number:");
+	scanf("%d", &a);
+	pintf("Type 2nd number");
+	scanf("%d", &b);
+	sum = a+b;
+	printf("%d", sum);
+	return 0;
+	
+	
+	}

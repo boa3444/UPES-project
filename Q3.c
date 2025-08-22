b4a21d997c9f60284e65e6d34968b8e8931079b7
boa3444 <newone1804@gmail.com>
2025-08-22 06:10:40 +0000

    DAY2

---
+++ Q3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int area, length, width, perimeter;
+    printf("Enter the length and width of the rectangle: ");
+    scanf("%d %d", &length, &width);
+    area = length * width;
+    perimeter = 2 * (length + width);
+    printf("Perimeter of the rectangle: %d\n", perimeter);  
+    printf("Area of the rectangle: %d\n", area);
+    return 0;
+}

a4d885ae96c3f0e60549ba62a0f060c5570b98eb
boa3444 <newone1804@gmail.com>
2025-08-24 13:20:50 +0000

    DAY4

---
+++ Q7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void){
+	int a,b;
+	int numbers[4] = {1,2};
+	printf("Type a:");
+	scanf("%d", &a);
+	numbers[0] = a;
+	printf("Type b:");
+	scanf("%d", &b);
+	numbers[1] = b;
+	a= numbers[1];
+	b= numbers[0];
+	printf("Your a:%d\n", a);
+	printf("Your b:%d",b);
+	return 0;
+	}

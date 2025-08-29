dbd43ca3a02ad17379fafc5501f4d52a0c553dae
boa3444 <newone1804@gmail.com>
2025-08-29 19:37:25 +0000

    DAY6

---
+++ Q12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int num = 0;
+	scanf("%d", &num);
+	if (num >= 0){
+		if (num == 0){
+			printf("Its ZERO");
+			}
+		else
+			printf("Its positive");
+			
+			}
+	else if (num < 0){
+		printf("Its NEGATIVE");
+		}
+		
+		return 0;
+		
+		}
+	

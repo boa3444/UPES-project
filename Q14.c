a3433c831a410493d9e128340052f8f3cd0b7a72
boa3444 <newone1804@gmail.com>
2025-08-29 20:03:38 +0000

    DAY7

---
+++ Q14.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+	char input;
+	char vowels[100] = {'A', 'E', 'I', 'O', 'U'};
+	int length = sizeof(vowels)/ sizeof(vowels[0]);
+	char letter = toupper((unsigned char) input);
+	printf("type a letter:");
+	scanf("%c", &input);
+	for ( int i = 0; i < length; i++){
+		if (input == vowels[i]){
+			printf("ITS A VOWEL");
+			break;
+			}
+		else
+			printf("NOT A VOWEL");
+			break;
+			}
+			
+	return 0 ;
+	}
